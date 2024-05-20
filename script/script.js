e4c6a0e45756e95fb78677cbd59aaa0ebe6b1b4f
jayden220 <jaydenvanrooyen87@gmail.com>
2024-05-20 12:16:43 +0200

    finished up

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
-let celsius = document.getElementsByClassName("degreesC")
-let calculate = document.getElementsByClassName('convert')
+let celsius = document.getElementsByTagName("input")[0]
+let convert = document.getElementsByClassName('convert')[0]
+let answer = document.getElementsByClassName('fahrenheit')[0]
+
+
+convert.addEventListener('click', () =>{
+    answer.innerText = (9/5 * celsius.value ) + 32 
+})
 
 
 
-function convertTemperature() {
+// function convertTemperature() {
     
-    let celsius = parseFloat(document.getElementById("degreesC").value);
-    let fahrenheit = (celsius * 9/5) + 32;
-    document.getElementsByClassName("result").innerText = fahrenheit.toFixed(2) + "°F";
-}
+//     let celsius = parseFloat(document.getElementById("degreesC").value);
+//     let  = (celsius * 9/5) + 32;
+//     document.getElementsByClassName('result').innerText = fahrenheit.toFixed(2) + "°F";
+// }
